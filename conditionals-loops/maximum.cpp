3eb30aa5ba3827440b01ea66d82d8aa654fef477
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-21 12:51:32 +0530

    Create maximum.cpp

---
+++ conditionals-loops/maximum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+
+    if(a>b) {
+        cout<<"A is greater"<<endl;
+    }
+    else {
+        cout<<"B is greater"<<endl;
+    }
+return 0;
+}
