f09c004daef97b56167e76bc84789283f0c31998
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-21 16:13:36 +0530

    basic pattern

---
+++ patterns/basic-pattern.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter the number of rows: ";
+    cin>>n;
+    int i = 0, j = 0;
+
+    while(i <= n) {
+        while(j <= n) {
+            cout<<"*"<<" ";
+            j = j + 1;
+        }
+        cout<<endl;
+        i = i + 1;
+    }
+return 0;
+}
