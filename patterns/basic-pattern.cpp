abff53358ceeddd52a9c8d42ac3a088b2c2fc336
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-21 16:17:40 +0530

    commit

--- patterns/basic-pattern.cpp
+++ patterns/basic-pattern.cpp
@@ -5,9 +5,10 @@ int main() {
     int n;
     cout<<"Enter the number of rows: ";
     cin>>n;
-    int i = 0, j = 0;
+    int i = 0;
 
     while(i <= n) {
+        int j = 0; // initialize inside
         while(j <= n) {
             cout<<"*"<<" ";
             j = j + 1;
