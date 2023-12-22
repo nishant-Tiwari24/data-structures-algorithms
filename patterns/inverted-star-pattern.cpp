1d7e8e96692e5e5f02f799f2fcc43c7d7c1f5d90
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-22 13:27:45 +0530

    Update inverted-star-pattern.cpp

--- patterns/inverted-star-pattern.cpp
+++ patterns/inverted-star-pattern.cpp
@@ -7,17 +7,18 @@ int main() {
     cin>>n;
     int i = 1;
     while(i <= n) {
-        int space = 1;
-        int j = 1;
-        while(space <= n) {
+        int space = n - i;
+        while(space) {
             cout<<" "<<" ";
             space = space + 1;
         }
-
+        
+        int j = 1;
         while(j <= i) {
-            cout<<j<<" ";
+            cout<<"*"<<" ";
             j = j + 1;
         }
+        cout<<endl;
         i = i + 1;
     }
     return 0;
