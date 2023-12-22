117275c85f135414bdb026dd74b6464cc2df6089
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-22 23:16:45 +0530

    operators

---
+++ operators-for-loop/bitwise-operators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 4;
+    int b = 7;
+
+    cout<<a&b<<endl;
+    cout<<a|b<<endl;
+    cout<<~a<<endl;
+    cout<<a^b<<endl;
+}
---
+++ operators-for-loop/leftshift-rightshift.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<< (9>>2) <<endl;
+    cout<< (8<<3) <<endl;
+
+    return 0;
+}
