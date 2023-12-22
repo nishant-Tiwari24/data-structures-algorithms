c5c4907c1c21d184cabb8fcc48afd05fd0317cce
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-23 00:13:20 +0530

    continue

---
+++ operators-for-loop/continue.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<"hey";
+        cout<<"hi";
+        continue; //skips the itreation
+        cout<<"no cout";
+    }
+    
+}
