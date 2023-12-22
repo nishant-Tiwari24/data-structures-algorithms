260c32debec59845e0905a721d950dc4a12f9cb3
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-22 14:00:29 +0530

    Update dabbang-pattern.cpp

--- patterns/dabbang-pattern.cpp
+++ patterns/dabbang-pattern.cpp
@@ -12,6 +12,29 @@ int main() {
             cout<<j<<" ";
             j = j - 1;
         }
-        
+
+        int k = 1;
+        while(k<=i) {
+            cout<<"*"<<" ";
+            k = k + 1;
+        }
+
+        int l = i - 1;
+        while(l) {
+            cout <<"*"<<" ";
+            l = l - 1;
+
+        }
+
+        int m = n;
+        int count = m;
+        while(m) {
+            cout<<count<<" ";
+            count = count - 1;
+            m = m - 1;
+        }
+        i = i + 1;
     }
+    cout<<endl;
+    return 0;
 }
