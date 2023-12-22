0638cda546cf679cd3a4fd0c7c6767485cf247e7
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-22 11:08:19 +0530

    right triangle

---
+++ patterns/right-triangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter the number of rows : ";
+    cin>>n;
+
+    int i = 1;
+    while(i<=n) {
+        int j = n - i + 1;
+        while(j<=n) {
+            cout<<"*"<<" ";
+            j = j + 1;
+        }
+        cout<<"\n";
+        i = i + 1;
+    }
+    return 0;
+}
