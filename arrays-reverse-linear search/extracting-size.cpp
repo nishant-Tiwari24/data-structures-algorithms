a52c6531a9454f2a1d8a5568c157943e356b1939
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-27 13:39:31 +0530

    array

---
+++ arrays-reverse-linear search/extracting-size.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int fifth[10];
+    int size = sizeof(fifth)/sizeof(int);
+    cout<<size<<endl;
+}
---
+++ arrays-reverse-linear search/initialiser.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {[0 ... 4] = 5};
+
+}
