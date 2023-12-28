9e61c5bb6ca3e90b217862c54adf11fbd6fe522e
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-29 01:29:34 +0530

    binary search

---
+++ binary-search/omptimized-binary-search.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+bool search(int arr[], int n, int target) {
+    int start = 0;
+    int end = n -  1;
+
+    int  mid = start + (end - start)/2;
+
+    while (start<=end)
+    {
+        if(mid == target) {
+            return true;
+        }
+        else if(target < mid) {
+            end = mid - 1;
+            mid = start + (end - start)/2;
+        }
+        else {
+            start = mid + 1;
+            mid = start + (end - start)/2;
+        }
+    }
+    return false;
+    
+}
+
+int main() {
+    int arr[10] = {2,3,4,5,6,7,8,9,10,12};
+    search(arr,10,4);
+}
