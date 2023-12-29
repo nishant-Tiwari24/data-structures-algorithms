2e9e381134a6b468ac74c5016b1e1546d10213b5
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-29 23:58:42 +0530

    binary search

---
+++ binary-search/first-last-occurance.cpp
@@ -0,0 +1,55 @@
+#include <iostream> 
+#include <vector>
+
+int first(vector<int>& arr, int n, int k) {
+    int s = 0;
+    int e = n - 1;
+    int ans = -1;
+
+    int m = e + (s-e)/2;
+    while (s <= e) {
+      if (k == arr[m]) {
+          ans = m;
+          e = m - 1;
+      }
+      else if (k < arr[m]) {
+          e = m - 1;
+      }
+      else {
+          s = m + 1;
+      }
+      m = e + (s-e)/2;
+    }
+    return ans;
+}
+
+int last(vector<int>& arr, int n, int k) {
+    int s = 0;
+    int e = n - 1;
+    int ans = -1;
+
+    int m = e + (s-e)/2;
+    while (s <= e) {
+      if (k == arr[m]) {
+          ans = m;
+          s = m + 1;
+      }
+      else if (k < arr[m]) {
+          e = m - 1;
+      }
+      else {
+          s = m + 1;
+      }
+      m = e + (s-e)/2;
+    }
+    return ans;
+}
+
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
+{
+    // Write your code here
+    pair<int,int> p;
+    p.first = first(arr, n, k);
+    p.second = last(arr, n, k);
+    return p;
+}
