9946a4126c30b3b71eb380a93999f295f1a57685
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-01-04 12:37:01 +0530

    binary search

---
+++ binary-search/pivot-element-array.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+using namespace std;
+
+int pivot(int arr[],int n) {
+    int s = 0;
+    int e = s -1;
+    int m = s + (e-s)/2;
+    
+    while(s<=e) {
+        if(arr[m] >= arr[0]) {
+            s = m + 1;
+        }
+        else {
+            e = m;
+        }
+    }
+    return s;
+}
