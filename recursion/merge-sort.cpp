cd917ac4ffc0b142fb22ab434ff0c97a4e952bc1
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-02-03 01:21:35 +0530

    recursion

--- recursion/merge-sort.cpp
+++ recursion/merge-sort.cpp
@@ -2,33 +2,56 @@
 using namespace std;
 
 void merge(int arr[], int s, int e) {
-    int m = s + (e-s)/2;
-    int l1 = m-s+1;
-    int l2 = e-m;
-    int *f = new int [l1];
-    int *s = new int [l2];
+    int m = s + (e - s) / 2;
+    int l1 = m - s + 1;
+    int l2 = e - m;
+    int *first = new int[l1];
+    int *second = new int[l2];
 
-    int k=s;
-    for(int i=0; i<l1; i++) {
-        f[i] =  arr[k++]; 
+    int mainArr = s;
+    for (int i = 0; i < l1; i++) {
+        first[i] = arr[mainArr++];
     }
-    k=m+1;
-    for(int i=0; i<l2; i++) {
-        s[i] = arr[k++];
+    mainArr = m + 1;
+    for (int i = 0; i < l2; i++) {
+        second[i] = arr[mainArr++];
     }
+
     int i1 = 0;
     int i2 = 0;
-    int main = s;
-    while(i1<l1 && i2<l2) {
-        if(f[i1]<s[i2])
+    mainArr = s;
+    while (i1 < l1 && i2 < l2) {
+        if (first[i1] < second[i2]) {
+            arr[mainArr++] = first[i1++];
+        } else {
+            arr[mainArr++] = second[i2++];
+        }
     }
-
+    while (i2 < l2) {
+        arr[mainArr++] = second[i2++];
+    }
+    while (i1 < l1) {
+        arr[mainArr++] = first[i1++];
+    }
+    delete[] first;
+    delete[] second;
 }
 
 void mergeSort(int arr[], int s, int e) {
-    if(s>e) return;
-    int m = s + (e-s)/2;
-    mergeSort(arr,s,m);
-    mergeSort(arr,m+1,e);
-    merge(arr,s,e);
+    if (s >= e) return;
+    int m = s + (e - s) / 2;
+    mergeSort(arr, s, m);
+    mergeSort(arr, m + 1, e);
+    merge(arr, s, e);
+}
+
+int main() {
+    int arr[5] = {2, 3, 1, 4, 5};
+    int n = 5;
+
+    mergeSort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+    return 0;
 }
