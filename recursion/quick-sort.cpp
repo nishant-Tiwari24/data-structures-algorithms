564d9163410086eaf098f03b198045c7ff0bd6ed
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-02-03 12:36:02 +0530

    recursion

--- recursion/quick-sort.cpp
+++ recursion/quick-sort.cpp
@@ -10,11 +10,11 @@ int pivot(int arr[], int s, int e) {
     swap(arr[s],arr[pivotIndex]);
     int i=s;int j=e;
     while(i<pivotIndex && j>pivotIndex) {
-        while(arr[i]<p) i++;
+        while(arr[i]<=p) i++;
         while(arr[j]>p) j--;
         if(i<pivotIndex&&j>pivotIndex) swap(arr[i++],arr[j--]);
     }
-    return pivotIndex
+    return pivotIndex;
 }
 
 void quickSort(int arr[], int s, int e) {
